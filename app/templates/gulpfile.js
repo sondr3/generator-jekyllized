d4a6033e696bf2c895ca6c279501daf8ec281ff2
Sondre Nilsen <nilsen.sondre@gmail.com>
2014-02-05 02:49:27 +0100

    Adds all the files and configurations for Gulp
    Adds a config.json file to read the directories from, updates the
    package.json file to include the plugins for Gulp and the gulpfile.js
    itself. Updates #40 and #39.

---
+++ app/templates/gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+// Generated on <%= (new Date).toISOString().split('T')[0] %> using <%= pkg.name %> <%= pkg.version %>
+
+var	gulp = require('gulp');
+var gutil = require('gulp-util');
+
+gulp.task('default', function () {
+	// body...
+});
