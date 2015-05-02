336e2693bc3f8b42d5d6ed37212b325a6e4149fb
Sondre Nilsen <nilsen.sondre@gmail.com>
2015-05-02 02:23:49 +0200

    Merged package subgen with app generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,10 +1,66 @@
 'use strict';
 
+var _ = require('lodash');
+var chalk = require('chalk');
 var generators = require('yeoman-generator');
 
 module.exports = generators.Base.extend({
-
   initializing: function() {
+    this.props = {};
+    this.pkg = this.fs.readJSON(this.destinationPath('package.json'), {});
+  },
+
+  prompting: function() {
+    var done = this.async();
+
+    var prompts = [{
+      name: 'projectName',
+      message: 'What is the name of your project?'
+    }, {
+      name: 'projectDescription',
+      message: 'Describe your project'
+    }, {
+      name: 'projectURL',
+      message: chalk.red('If you are using GHPages use username.github.io') +
+        '\nWhat will the URL for your project be?'
+    }, {
+      name: 'authorName',
+      message: 'What\'s your name?'
+    }, {
+      name: 'authorEmail',
+      message: 'What\'s your email?'
+    }, {
+      name: 'authorBio',
+      message: 'Write a short description about yourself'
+    }, {
+      name: 'authorTwitter',
+      message: 'Your Twitter handle'
+    }];
+
+    this.prompt(prompts, function(props) {
+      this.props = _.extend(this.props, props);
+      this.config.set(this.props);
+
+      done();
+    }.bind(this));
+  },
+
+  writing: function() {
+    var pkgJSONFields = {
+      name: _.kebabCase(this.props.projectName),
+      version: '0.0.0',
+      description: this.props.projectDescription,
+      homepage: this.props.projectURL,
+      author: {
+        name: this.props.authorName,
+        email: this.props.authorEmail
+      }
+    };
+
+    this.fs.writeJSON('package.json', _.extend(pkgJSONFields, this.pkg));
+  },
+
+  default: function() {
     this.composeWith('jekyllized:editorconfig', {}, {
       local: require.resolve('../editorconfig')
     });
@@ -21,8 +77,8 @@ module.exports = generators.Base.extend({
       local: require.resolve('../git')
     });
 
-    this.composeWith('jekyllized:package', {}, {
-      local: require.resolve('../package')
+    this.composeWith('jekyllized:gulp', {}, {
+      local: require.resolve('../gulp')
     });
   }
 });
--- test/app.js
+++ test/app.js
@@ -1,29 +1,89 @@
 'use strict';
 
+var _ = require('lodash');
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
+var fs = require('fs');
+
+function assertObjectContains(obj, content) {
+  Object.keys(content).forEach(function(key) {
+    if (typeof content[key] === 'object') {
+      assertObjectContains(content[key], obj[key]);
+    } else {
+      assert.equal(content[key], obj[key]);
+    }
+  });
+}
+
+function assertJSONFileContains(filename, content) {
+  var obj = JSON.parse(fs.readFileSync(filename, 'utf8'));
+  assertObjectContains(obj, content);
+}
 
 describe('jekyllized:app', function() {
-  before(function(done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .on('ready', function(gen) {
-        gen.fs.copy(
-            path.join(__dirname, '../package.json'),
-            gen.destinationPath('package.json')
-          );
-      })
-      .on('end', done);
+  describe('running on new project', function() {
+    before(function(done) {
+      this.answers = {
+        projectName: 'jekyllized',
+        projectDescription: 'Test site for Jekyllized',
+        projectURL: 'www.test.com',
+        authorName: 'Ola Nordmann',
+        authorEmail: 'ola.nordmann@gmail.com'
+      };
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts(this.answers)
+        .on('end', done);
+    });
+
+    it('creates files', function() {
+      assert.file([
+        '.editorconfig',
+        '.jshintrc',
+        '.jscsrc',
+        '.gitignore',
+        '.gitattributes',
+        'package.json'
+      ]);
+    });
+
+    it('creates package.json', function() {
+      assert.file('package.json');
+      assertJSONFileContains('package.json', {
+        name: this.answers.projectName,
+        version: '0.0.0',
+        description: this.answers.projectDescription,
+        homepage: this.answers.projectURL,
+        author: {
+          name: this.answers.authorName,
+          email: this.answers.authorEmail
+        },
+      });
+    });
   });
 
-  it('creates files', function() {
-    assert.file([
-      '.editorconfig',
-      '.jshintrc',
-      '.jscsrc',
-      '.gitignore',
-      '.gitattributes',
-      'package.json'
-    ]);
+  describe('running on existing project', function() {
+    before(function(done) {
+      this.pkg = {
+        version: '3.1.4',
+        description: '404 not found',
+        homepage: 'ulv.no',
+        author: 'Kari Nordmann'
+      };
+
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          projectName: 'jekyllized'
+        })
+        .on('ready', function(gen) {
+          gen.fs.writeJSON(gen.destinationPath('package.json'), this.pkg);
+        }.bind(this))
+        .on('end', done);
+    });
+
+    it('creates package.json', function() {
+      var pkg = _.extend({name: 'jekyllized'}, this.pkg);
+      assertJSONFileContains('package.json', pkg);
+    });
   });
 });
