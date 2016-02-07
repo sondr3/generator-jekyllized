dd749de4c00f910329325ddf439b630a8226e04d
Sondre Nilsen <nilsen.sondre@gmail.com>
2016-02-07 16:16:15 +0100

    Remove bower subgenerator
    It's not used nor needed anymore

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 module.exports = {
   app: require.resolve('./generators/app'),
   boilerplate: require.resolve('./generators/boilerplate'),
-  bower: require.resolve('./generators/bower'),
   gulp: require.resolve('./generators/gulp'),
   jekyll: require.resolve('./generators/jekyll')
 };
