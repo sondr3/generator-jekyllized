ee3264d1b5deaf3c4ff6fb51f5a21f344da014d1
Sondre Nilsen <nilsen.sondre@gmail.com>
2016-04-23 17:13:37 +0200

    Fix URLs needing HTTP(s)
    This also fixes #124.

--- generators/app/index.js
+++ generators/app/index.js
@@ -50,6 +50,13 @@ module.exports = generators.Base.extend({
       name: 'projectURL',
       message: chalk.yellow('If you will be using Github Pages, use username.github.io\n') +
         chalk.yellow('? ') + 'What will the URL for your project be?',
+      validate: function (input) {
+        if (input.startsWith('http')) {
+          return true;
+        }
+
+        return 'URL must contain either HTTP or HTTPs';
+      },
       store: true
     }, {
       name: 'authorName',
