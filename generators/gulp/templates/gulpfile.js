3a142ef021fb61439e686b8e7a63e7d2ede32b72
Sondre Nilsen <nilsen.sondre@gmail.com>
2016-05-29 14:21:53 +0200

    Fix syntax for gulp-imagemin

--- generators/gulp/templates/gulpfile.js
+++ generators/gulp/templates/gulpfile.js
@@ -163,10 +163,12 @@ gulp.task('inject:footer', () =>
 // 'gulp images' -- optimizes and caches your images
 gulp.task('images', () =>
   gulp.src('src/assets/images/**/*')
-    .pipe($.cache($.imagemin({
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({progressive: true})
-    })))
+    .pipe($.cache($.imagemin([
+      $.imagemin.gifsicle({interlaced: true}),
+      $.imagemin.jpegtran({progressive: true}),
+      $.imagemin.optipng(),
+      $.imagemin.svgo({plugins: [{cleanupIDs: false}]})
+    ])))
     .pipe(gulp.dest('.tmp/assets/images'))
     .pipe($.size({title: 'images'}))
 );
