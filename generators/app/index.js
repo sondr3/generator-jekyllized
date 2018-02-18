b7e6343c07df46e3adcee26715a132967a9a81ad
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-02-18 02:26:36 +0100

    should not fail

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,6 +4,13 @@ const chalk = require("chalk");
 const yosay = require("yosay");
 
 module.exports = class extends Generator {
+  writing() {
+    this.fs.copy(
+      this.templatePath("README.md"),
+      this.destinationPath("README.md")
+    );
+  }
+
   default() {
     this.composeWith(require.resolve("generator-node/generators/editorconfig"));
     this.composeWith(require.resolve("generator-node/generators/git"));
