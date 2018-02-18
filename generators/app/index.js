b343fb60fbfa6906e5ca5de8639efdad3b58d758
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-02-18 02:22:48 +0100

    should be failing

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,25 +1,18 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
 
 module.exports = class extends Generator {
-  writing() {
-    this.fs.copy(
-      this.templatePath('README.md'),
-      this.destinationPath('README.md')
-    );
-  }
-
   default() {
-    this.composeWith(require.resolve('generator-node/generators/editorconfig'));
-    this.composeWith(require.resolve('generator-node/generators/git'));
+    this.composeWith(require.resolve("generator-node/generators/editorconfig"));
+    this.composeWith(require.resolve("generator-node/generators/git"));
 
-    this.composeWith(require.resolve('../jekyll'));
+    this.composeWith(require.resolve("../jekyll"));
   }
 
   install() {
     this.npmInstall();
-    this.runInstall('bundle');
+    this.runInstall("bundle");
   }
 };
