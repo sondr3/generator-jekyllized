19b79e60ec93c3515568024aacb1f4697b8354ab
Sondre Nilsen <nilsen.sondre@gmail.com>
2018-02-19 01:08:28 +0100

    Don't require removed packages

--- generators/git/index.js
+++ generators/git/index.js
@@ -1,7 +1,5 @@
 "use strict";
 const Generator = require("yeoman-generator");
-const chalk = require("chalk");
-const yosay = require("yosay");
 
 module.exports = class extends Generator {
   constructor(args, options) {
